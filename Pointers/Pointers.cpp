b1d816e1cf47016158b5222cd5fa4058b536a3d8
Ming_tc51 <Ming_tc51@itstepru.onmicrosoft.com>
2021-11-01 21:58:46 +0500

    Добавьте файлы проекта.

---
+++ Pointers/Pointers.cpp
@@ -0,0 +1,32 @@
+﻿// Pointers.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+//#define POINTERS_BASICS
+#define POINTERS_AND_ARRAYS
+void main()
+{
+    setlocale(LC_ALL, " ");
+#ifdef POINTERS_BASICS // Inactive Preprocessor Block
+    //Если определено POINTERS_BASICS, то ниже следующий код, до директивы #endif
+    int a = 2;
+    int* pa = &a;
+    std::cout << a << std::endl;// Значение 'a' 
+    std::cout << &a << std::endl;// Адрес 'a'
+    std::cout << pa << std::endl; // Адрес 'a' хранящийся в указателе 'pa'
+    std::cout << &pa << std::endl;// Выводим адрес 'pa' 
+    std::cout << *pa << std::endl;
+#endif // POINTERS_BASICS
+
+#ifdef POINTERS_AND_ARRAYS
+    const int n = 5;
+    int arr[n] = { 3,5,8,13,21 };
+    std::cout << *arr << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout<<*(arr + i) << "\t";
+    }
+#endif // POINTERS_AND_ARRAYS
+
+
+}
---
+++ Transfer parametrs/Transfer parametrs.cpp
@@ -0,0 +1,44 @@
+﻿// Transfer parametrs.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+void Exchange(int& a, int& b);
+void FillRand(int arr[], const unsigned int b);
+void Print(int arr[], const unsigned int b);
+void main()
+{
+    setlocale(LC_ALL, " ");
+    int a = 2;
+    int b = 3;
+    std::cout << a << "\t" << b << std::endl;
+    Exchange(a, b);
+    std::cout << a << "\t" << b << std::endl;
+
+    /*const int b = 5;
+    int arr[b];
+    FillRand(arr, b);
+    Print(arr, b);*/
+    
+}
+void Exchange(int& a, int& b)
+{
+    int buffer = a;
+    a = b;
+    b = buffer;
+}
+void FillRand(int arr[], const unsigned int b)
+{
+    for (int i = 0; i < b; ++i)
+    {
+        *(arr + i) = rand() % 100;
+    }
+
+}
+void Print(int arr[], const unsigned int b)
+{
+    for (int i = 0; i < b; ++i)
+    {
+        std::cout << arr[i] << "\t";
+    }
+    std::cout << std::endl;
+}
